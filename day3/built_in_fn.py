fe49fac44f7aa2a39924e6bc35f77cc2b24ee3d9
kiyongee2 <kiyongee2@gmail.com>
2023-04-08 13:20:47 +0900

    first commit

---
+++ day3/built_in_fn.py
@@ -0,0 +1,21 @@
+# 내장 함수
+# 절대값
+print(abs(4))
+print(abs(-4))
+
+# 반올림 round(number, 자리수)
+print(round(2.56)) #정수로 반올림
+print(round(2.56, 1))  #소수 첫째자리로 반올림
+print(round(1256, -1)) #일의 자리에서 반올림
+
+# 최대값 - max()
+max_v = max([3, 1, 8])
+min_v = min([3, 1, 8])
+print(f'최대값 : {max_v}')
+print(f'최소값 : {min_v}')
+
+# 버림 - floor()
+#floor()
+
+
+
---
+++ day3/module2.py
@@ -0,0 +1,4 @@
+from price import get_price
+
+money = get_price(10000, 1)
+print(money)
